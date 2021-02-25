38c1d3a5f065b15140ef143ff886fb0ad6a4e807
ziyi-bear <m0724001@gm.nuu.edu.tw>
2021-02-25 12:27:53 +0800

    0.0

--- app/src/main/java/com/experto/springbootpostgresqldocker/controller/Home.java
+++ app/src/main/java/com/experto/springbootpostgresqldocker/controller/Home.java
@@ -8,7 +8,7 @@ public class Home {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Spring Boot with Docker";
+        return "{網站URL}/user/all: 會顯示全部資料庫內的使用者 <br> {網站URL}/user/{id}: 會顯示該id的使用者資訊";
     }
 
 }
--- db/user_model.sql
+++ db/user_model.sql
@@ -1,18 +1,5 @@
 -- Adminer 4.8.0 PostgreSQL 13.2 (Debian 13.2-1.pgdg100+1) dump
 
-<br />
-<b>Warning</b>:  PDO::query(): SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near &quot;STATUS&quot;
-LINE 1: SHOW FUNCTION STATUS WHERE Db = 'test'
-                      ^ in <b>/var/www/html/adminer.php</b> on line <b>185</b><br />
-<br />
-<b>Warning</b>:  PDO::query(): SQLSTATE[42601]: Syntax error: 7 ERROR:  syntax error at or near &quot;STATUS&quot;
-LINE 1: SHOW PROCEDURE STATUS WHERE Db = 'test'
-                       ^ in <b>/var/www/html/adminer.php</b> on line <b>185</b><br />
-<br />
-<b>Warning</b>:  PDO::query(): SQLSTATE[42703]: Undefined column: 7 ERROR:  column &quot;consrc&quot; does not exist
-LINE 1: SELECT conname, consrc
-                        ^
-HINT:  Perhaps you meant to reference the column &quot;pg_constraint.conkey&quot; or the column &quot;pg_constraint.conbin&quot;. in <b>/var/www/html/adminer.php</b> on line <b>185</b><br />
 DROP TABLE IF EXISTS "user_model";
 CREATE TABLE "public"."user_model" (
     "id" bigint NOT NULL,
